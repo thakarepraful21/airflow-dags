fa3baf268b19cc6b6e8f8a42eccdf910690874bb
Praful Sahebrao Thakare <prafult@cybage.com>
2025-08-06 18:50:46 +0530

    Corrected the dag_id

--- test_dag.py
+++ test_dag.py
@@ -2,31 +2,36 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
+# Default arguments for the DAG
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2025, 8, 1),
+    'start_date': datetime(2023, 1, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-with DAG(
-    'test_dag',
+# Define the DAG
+dag = DAG(
+    dag_id='test_dag',
     default_args=default_args,
     description='Simple test DAG',
     schedule_interval='@daily',
     catchup=False,
     tags=['example'],
-) as dag:
+)
 
-    print_date = BashOperator(
-        task_id='print_date',
-        bash_command='date',
-    )
+# Define the first task
+print_date = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
 
-    sleep_task = BashOperator(
-        task_id='sleep_task',
-        bash_command='sleep 5',
-    )
+# Define the second task
+sleep_task = BashOperator(
+    task_id='sleep_task',
+    bash_command='sleep 5',
+    dag=dag,
+)
 
-    print_date >> sleep_task
+# Set task dependencies
+print_date >> sleep_task
